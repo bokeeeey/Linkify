93246d9df10f9483f093f80c41b8e24b6a3380e9
김보경 <rlaqhrud6263@naver.com>
2024-09-30 23:54:01 +0900

    Setting: eslint 관련 추가 설정 > 의존성 추가

--- apps/react/eslint.config.js
+++ apps/react/eslint.config.js
@@ -7,7 +7,11 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "@repo/eslint-config/react-internal.js",
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,10 +23,17 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "warn", // 'any' 타입 사용 억제
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ], // 사용되지 않는 변수 규칙
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+      "react/prop-types": "off", // TypeScript 사용 시 prop-types를 끄는 설정
+      "react/react-in-jsx-scope": "off", // React 17+에서는 import가 필요 없음
     },
   }
 );
