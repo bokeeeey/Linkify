11350bb2f213736966ab0cdead0a249b68ca66ec
김보경 <rlaqhrud6263@naver.com>
2024-10-01 00:37:32 +0900

    Setting: tailwind 의존성 가장 최상위로 이동

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="text-custom-blue">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -14,9 +14,12 @@ export default function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-custom-blue">
         Click on the Vite and React logos to learn more
       </p>
+      <main>
+        <div className="text-custom-blue">테일윈드 확인용</div>
+      </main>
     </>
   );
 }
