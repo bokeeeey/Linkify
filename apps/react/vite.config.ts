2ebf93b286079fffc392fc7f934fe09a56f4baf3
김보경 <rlaqhrud6263@naver.com>
2024-10-01 23:36:00 +0900

    Setting: svgr & svgo 설정 추가

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,10 +1,29 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import svgr from '@svgr/rollup';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgo: true, // SVGO를 사용할지 여부
+      svgoConfig: {
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeViewBox: false, // viewBox 속성 유지
+              },
+            },
+          },
+        ],
+      },
+      titleProp: true, // title 속성 사용 가능
+    }),
+  ],
   css: {
-    postcss: "./postcss.config.js",
+    postcss: './postcss.config.js',
   },
 });
